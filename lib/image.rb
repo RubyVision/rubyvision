b01e2004310886381d00fc8db0ff086eeb6014ef
harshadixit12 <harshadixit12@gmail.com>
2020-02-08 17:53:13 +0530

    gem structure

---
+++ lib/image.rb
@@ -0,0 +1,12 @@
+# Language: Ruby, Level: Level 3
+# frozen_string_literal = true
+# contains the definition of image class
+
+class Image
+  def initialize()
+    @pixel_array = []
+    @height = 0
+    @width = 0
+    @channels = 0   
+  end
+end
---
+++ lib/imgp.rb
@@ -0,0 +1,25 @@
+# Language: Ruby, Level: Level 3
+require 'image'
+class Imgproc
+ 
+
+  def bgr_to_grayscale(img)
+    
+  end
+
+  def bgr_to_hsv(img)
+    
+  end
+
+  def bgr_to_hsv_util(array)
+    
+  end
+  
+  
+  
+  
+
+
+
+
+end

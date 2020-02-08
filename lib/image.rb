21a9b8f74211923502b746dcbc2b52ce0433e563
harshadixit12 <harshadixit12@gmail.com>
2020-02-08 22:49:44 +0530

    changed access of image members to read

--- lib/image.rb
+++ lib/image.rb
@@ -3,7 +3,10 @@
 # contains the definition of image class
 require 'bmp'
 class Image
-
+  attr_reader :data
+  attr_reader :height
+  attr_reader :width
+  attr_reader :channels
   def initialize()
     @data = []
     @height = 0
@@ -12,18 +15,11 @@ class Image
   end
 
   def read(path)
-    
+
   end
 
   def write(path, name, format)
   end
-  attr_reader :data
-  attr_writer :data
-  attr_reader :height
-  attr_writer :height
-  attr_reader :width
-  attr_writer :width
-  attr_reader :channels
-  attr_writer :channels
+
 
 end
